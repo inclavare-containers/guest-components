b4b7dbc3a2c4b80bb9c310748c20b5f304001fa4
Jiale Zhang <zhangjiale@linux.alibaba.com>
2025-06-06 15:29:56 +0800

    Fix restful-as URL from TRUSTEE_URL ENV
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- attestation-agent/attestation-agent/src/token/coco_as.rs
+++ attestation-agent/attestation-agent/src/token/coco_as.rs
@@ -60,7 +60,14 @@ impl GetToken for CoCoASTokenGetter {
 
 impl CoCoASTokenGetter {
     pub fn new(config: &CoCoASConfig) -> Self {
-        let as_uri = std::env::var("TRUSTEE_URL").unwrap_or_else(|_| config.url.clone());
+        let as_uri = match std::env::var("TRUSTEE_URL") {
+            Result::Err(_) => config.url.clone(),
+            // This URL is point to the trustee gateway,
+            // We need to add the /attestation-service path to the URL.
+            // If we want to access the restful attestation service directly,
+            // now only support via config file.
+            Result::Ok(env_url) => format!("{}/attestation-service", env_url),
+        };
         Self { as_uri }
     }
 }
