c67f101db03649dbd346595c3c0a7642da6e5f4a
Jiale Zhang <zhangjiale@linux.alibaba.com>
2026-01-22 16:36:24 +0800

    Add aa-instance-type config and eventlog enable to AA start script
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- aa-start.sh
+++ aa-start.sh
@@ -6,11 +6,13 @@ set -o noglob
 # Initialize parameters
 trustee_address=''
 as_addr=''
+aa_instance_type=''
 
 usage() {
   echo "This script is used to start Attestation Agent" 1>&2
   echo "" 1>&2
   echo "Usage: $0 --trustee-addr Address of remote trustee" 1>&2
+  echo "          --aa-instance-type [aliyun_ecs|aliyun_eas] (optional) Attestation Agent instance type" 1>&2
 
   exit 1
 }
@@ -24,6 +26,10 @@ while [[ "$#" -gt 0 ]]; do
       ;;
     --trustee-addr)
       trustee_address="$2"
+      shift 2
+      ;;
+    --aa-instance-type)
+      aa_instance_type="$2"
       shift 2
       ;;
     -h|--help)
@@ -43,6 +49,16 @@ url = "${as_addr}"
 
 [token_configs.kbs]
 url = "${trustee_address}"
+
+[eventlog_config]
+enable_eventlog = true
 EOF
+
+if [[ -n "${aa_instance_type}" ]]; then
+  cat << EOF >> /etc/attestation-agent.toml
+[aa_instance]
+instance_type = "${aa_instance_type}"
+EOF
+fi
 
 attestation-agent -c /etc/attestation-agent.toml
