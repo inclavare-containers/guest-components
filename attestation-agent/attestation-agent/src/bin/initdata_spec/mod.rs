42e52a876cf1791f7a0f922f2e7a44d49517fa6e
Xynnn007 <xynnn@linux.alibaba.com>
2026-06-09 14:53:52 +0800

    initdata-processor: move to attestation-agent
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-agent/attestation-agent/src/bin/initdata_spec/mod.rs
+++ attestation-agent/attestation-agent/src/bin/initdata_spec/mod.rs
@@ -5,13 +5,13 @@
 // Aligned with kata-containers `kata_types::initdata::InitData` for parsing guest initdata TOML.
 
 use anyhow::{anyhow, Result};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 const INITDATA_VERSION: &str = "0.1.0";
 const SUPPORTED_ALGORITHMS: [&str; 3] = ["sha256", "sha384", "sha512"];
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub struct InitData {
     version: String,
